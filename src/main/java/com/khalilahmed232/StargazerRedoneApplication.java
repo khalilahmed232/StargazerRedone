e9c8dcf7d7200917345eca4ddd125f46205a9f17
Khalil Ahmed <khalilahmed232@gmail.com>
2021-04-10 12:39:00 +0530

    initial commit

---
+++ src/main/java/com/khalilahmed232/StargazerRedoneApplication.java
@@ -0,0 +1,13 @@
+package com.khalilahmed232;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargazerRedoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargazerRedoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khalilahmed232/StargazerRedoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalilahmed232;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargazerRedoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
